c927cdefffa0bf2ef488132c26f3298a164874a0
MrWetsnow <509887+DanTulovsky@users.noreply.github.com>
2024-04-16 16:57:35 -0400

    updates

--- setup_new.sh
+++ setup_new.sh
@@ -67,14 +67,18 @@ function docker_linux_install() {
   sudo curl -fsSL https://download.docker.com/linux/"${dist}"/gpg -o /etc/apt/keyrings/docker.asc
   sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+  if [[ ! -e /etc/apt/sources.list.d/docker.list ]]; then
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/${dist} \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+  else
+    echo "Docker repository already added"
+    return
+  fi
   # Add the repository to Apt sources:
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/${dist} \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update
   sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
   sudo usermod -aG docker "$USER"
 }
 
