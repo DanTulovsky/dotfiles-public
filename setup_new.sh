640187bf7b911d7765292c599bfd89be2f660dd0
MrWetsnow <509887+DanTulovsky@users.noreply.github.com>
2024-04-16 12:03:49 -0400

    more

--- setup_new.sh
+++ setup_new.sh
@@ -45,6 +45,8 @@ docker_debian_linux_install() {
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update
+
+  sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
 
 docker_ubuntu_linux_install() {
