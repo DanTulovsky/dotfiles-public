c096b6ce8c98314330324567e07aee6063fe7b2a
MrWetsnow <509887+DanTulovsky@users.noreply.github.com>
2024-04-16 09:19:59 -0400

    cleanup

--- setup_new.sh
+++ setup_new.sh
@@ -8,18 +8,16 @@ linux_required_commands="ssh-askpass"
 
 # all OS
 for com in ${required_commands}; do
-  if command -v $com >/dev/null 2>&1; then
-          echo "$com available"
+  if command -v "${com}" >/dev/null 2>&1; then
+          echo "${com} available"
   else
-          echo "$com is required"
+          echo "${com} is required"
           if uname -o |grep -i linux; then
-            sudo apt install $com
-            if [ $? != 0 ]; then
+            if ! sudo apt install "${com}"; then
               exit 1
             fi
           elif uname -o |grep -i darwin; then
-            brew install $com
-            if $! != 0; then
+            if ! brew install "${com}"; then
               exit 1
             fi
           else
@@ -35,8 +33,7 @@ for com in ${linux_required_commands}; do
   else
     echo "$com is required"
     if uname -o |grep -i linux; then
-      sudo apt install $com
-      if [ $? != 0 ]; then
+      if ! sudo apt install $com; then
         exit 1
       fi
     fi
@@ -44,17 +41,19 @@ for com in ${linux_required_commands}; do
 done
 
 echo "Installing pyenv..."
-rm -rf ${HOME}/.pyenv
+rm -rf "${HOME}"/.pyenv
 if uname -o |grep -i darwin; then
   brew install pyenv pyenv-virtualenv
 else
   curl https://pyenv.run |bash
 fi
 
+echo "Installing cargo..."
+curl https://sh.rustup.rs -sSf | sh
 
-if ! echo ${SHELL} |grep zsh >/dev/null 2>&1; then
+if ! echo "${SHELL}" |grep zsh >/dev/null 2>&1; then
   echo "Setting default shell to zsh..."
-  sudo -u $USER chsh -s $(which zsh)
+  sudo -u "$USER" chsh -s "$(which zsh)"
   echo "Please restart shell to switch to zsh and run this again..."
   exit 1
 fi
@@ -62,17 +61,17 @@ fi
 # setup ssh key
 git_identity_file="${HOME}/.ssh/identity.git"
 
-if [ ! -f ${git_identity_file} ]; then
+if [ ! -f "${git_identity_file}" ]; then
   echo "Generating ssh key for github into ${git_identity_file}"
-  ssh-keygen -f ${git_identity_file}
+  ssh-keygen -f "${git_identity_file}"
   echo "Add this key to github before continuing: https://github.com/settings/keys"
   echo ""
-  echo "$(cat ${git_identity_file}.pub)"
+  cat "${git_identity_file}".pub
   exit 1
 fi
 
 echo "Removing old config..."
-rm -rf $HOME/.cfg
+rm -rf "$HOME"/.cfg
 alias config='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 if ! grep ".cfg" .gitignore >/dev/null 2>&1; then
@@ -81,15 +80,16 @@ fi
 
 echo "Starting ssh agent..."
 keychain --nogui ~/.ssh/identity.git
-source ~/.keychain/$(hostname)-sh
+# shellcheck disable=SC1090
+source ~/.keychain/"$(hostname)"-sh
 
 echo "Cloning dotfiles..."
-git clone --bare git@github.com:DanTulovsky/dotfiles-config.git ${ZDOTDIR:-$HOME}/.cfg
+git clone --bare git@github.com:DanTulovsky/dotfiles-config.git "${ZDOTDIR:-$HOME}"/.cfg
 config reset --hard HEAD
 config config --local status.showUntrackedFiles no
 
 # install zprezto
-rm -rf ${ZDOTDIR:-$HOME}/.zprezto
+rm -rf "${ZDOTDIR:-$HOME}"/.zprezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 # install fonts
