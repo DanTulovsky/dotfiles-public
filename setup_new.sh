f0f19bff5225ca6a32aba3ebc134ace91c595aa8
MrWetsnow <509887+DanTulovsky@users.noreply.github.com>
2024-04-18 21:45:27 -0400

    fix

--- setup_new.sh
+++ setup_new.sh
@@ -18,6 +18,8 @@ function add_hashicorp_repo() {
  wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
  sudo apt update
+ sud
+  apt update
 }
 
 # language servers
@@ -77,7 +79,7 @@ function docker_linux_install() {
   else
     echo "Docker repository already added"
   fi
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   sudo usermod -aG docker "$USER"
 }
 
