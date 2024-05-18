f5d0218d93bf1e7f90d41954c1d2cd5a823a7cf8
MrWetsnow <dant@wetsnow.com>
2024-05-18 13:08:04 -0400

    Update setup_new.sh

--- setup_new.sh
+++ setup_new.sh
@@ -18,8 +18,6 @@ function add_hashicorp_repo() {
  wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
  sudo apt update
- sud
-  apt update
 }
 
 # language servers
