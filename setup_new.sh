e4f17a9076dbebc10dfea8d61b24de529ef1b331
MrWetsnow <dant@wetsnow.com>
2024-05-25 15:03:35 -0400

    Update setup_new.sh

--- setup_new.sh
+++ setup_new.sh
@@ -136,6 +136,12 @@ function is_arm_linx() {
   return $?
 }
 
+if is_darwin; then
+  if command -v brew > /dev/null 2>&1; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+fi
+
 for com in ${required_commands}; do
   if command -v "${com}" >/dev/null 2>&1; then
     echo "${com} available"
