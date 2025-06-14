ec6e5d2f417496dc465108caaa55cc6babb3aa90
MrWetsnow <dant@wetsnow.com>
2025-06-14 16:00:05 -0400

    Update setup_new.sh

--- setup_new.sh
+++ setup_new.sh
@@ -87,7 +87,7 @@ gcloud_linux_install() {
   fi
   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
   echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-  sudo apt-get update && sudo apt-get install google-cloud-cli kubectl
+  sudo apt-get update && sudo apt-get install -y google-cloud-cli kubectl
 }
 
 krew_install_plugins() {
