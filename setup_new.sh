444f6e7322335a4980b9532dec0a44cc86784639
Dan Tulovsky <dant@wetsnow.com>
2025-12-10 15:30:29 -0500

    more fixes

--- setup_new.sh
+++ setup_new.sh
@@ -561,12 +561,13 @@ function krew_install_plugins() {
     OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
     ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
     KREW="krew-${OS}_${ARCH}" &&
-    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-    tar zxvf "${KREW}.tar.gz" &&
-    ./"${KREW}" install krew
+    execute curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+    execute tar zxf "${KREW}.tar.gz" &&
+    execute ./"${KREW}" install krew
   )
   hash -r
-  ~/.krew_plugins || log_warn "Failed to run .krew_plugins"
+  log_info "Running .krew_plugins..."
+  execute ~/.krew_plugins || log_warn "Failed to run .krew_plugins"
 }
 
 function install_lazygit() {
