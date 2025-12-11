1e7fd052df06477db1a56d9868cf09c873e7a04d
Dan Tulovsky <dant@wetsnow.com>
2025-12-10 21:24:54 -0500

    cleabner

--- setup_new.sh
+++ setup_new.sh
@@ -775,7 +775,7 @@ EOM
   fi
 
   execute bash -c "curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg"
-  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list > /dev/null
   if execute sudo apt-get update && execute sudo apt-get install -y google-cloud-cli kubectl; then
     log_success
   else
